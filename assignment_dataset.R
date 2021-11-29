812bea61a9160fb0ecadbb595d5003e8dfdd87bf
Denise van Baalen <denisevbaalen@gmail.com>
2021-11-29 22:05:59 +0100

    Prediction attempts

--- assignment_dataset.R
+++ assignment_dataset.R
@@ -122,3 +122,44 @@ fg <- lavaanToGraph(fit, digits=2)
 cg <- coordinates(g)
 coordinates(fg) <- cg
 plot(fg, show.coefficients=TRUE)
+
+### ISOLATED EFFECTS
+library(bnlearn)
+net1 <- model2network(toString(g,"bnlearn"))
+df_scaled <- as.data.frame(scale(selected_df))
+fit1 <- bn.fit( net1, df_scaled )
+
+# FAMILY
+
+# adjustmentSets(g,"famrel","G3")
+summary( lm( G3 ~ famrel + famsup + Pstatus, as.data.frame(scale(selected_df)) ) )
+## Attempting to predict G3 from a single variable. Doesn't work great hahah.
+# predicted.G3 <- predict(fit1,node="G3",data=df_scaled[,"famrel",drop=F],method="bayes-lw")
+# plot(df_scaled[,"G3"],predicted.G3)
+# predicted.G3 <- predict(fit1,node="G3",data=df_scaled[,"famsup",drop=F],method="bayes-lw")
+# plot(df_scaled[,"G3"],predicted.G3)
+# predicted.G3 <- predict(fit1,node="G3",data=df_scaled[,"Pstatus",drop=F],method="bayes-lw")
+# plot(df_scaled[,"G3"],predicted.G3)
+
+## Attempting to predict G3 from a group of variables. No idea if this approach is valid.
+predicted.G3 <- predict(fit1,node="G3",data=df_scaled[,c("Pstatus","famrel","famsup"),drop=F],method="bayes-lw")
+plot(df_scaled[,"G3"],predicted.G3)
+
+
+# SCHOOL
+
+summary( lm( G3 ~ schoolsup + absences + failures, as.data.frame(scale(selected_df)) ) )
+predicted.G3 <- predict(fit1,node="G3",data=df_scaled[,c("schoolsup","absences","failures"),drop=F],method="bayes-lw")
+plot(df_scaled[,"G3"],predicted.G3)
+
+# SOCIAL
+
+summary( lm( G3 ~ Dalc + Walc + activities + freetime + goout, as.data.frame(scale(selected_df)) ) )
+predicted.G3 <- predict(fit1,node="G3",data=df_scaled[,c("Dalc","Walc","activities","freetime","goout"),drop=F],method="bayes-lw")
+plot(df_scaled[,"G3"],predicted.G3)
+
+# INTRINSIC
+
+summary( lm( G3 ~ higher, as.data.frame(scale(selected_df)) ) )
+predicted.G3 <- predict(fit1,node="G3",data=df_scaled[,"higher",drop=F],method="bayes-lw")
+plot(df_scaled[,"G3"],predicted.G3)
